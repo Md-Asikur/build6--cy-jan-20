fcd5c6d6f521144a19be8f721f53c4167c4487de
Md-Asikur <asikurrahaman997@gamil.com>
2023-01-20 06:50:36 -0800

    hello

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -4,16 +4,14 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 exports.processPayment = catchAsyncErrors(async (req, res, next) => {
   const myPayment = await stripe.paymentIntents.create({
-    amount: req.body.amount,
+    amount: req?.body?.amount,
     currency: "inr",
     metadata: {
       company: "Ecommerce",
     },
   });
 
-  res
-    .status(200)
-    .json({ success: true, client_secret: myPayment.client_secret });
+  res.status(200).json({ success: true, client_secret: myPayment.client_secret });
 });
 
 exports.sendStripeApiKey = catchAsyncErrors(async (req, res, next) => {
